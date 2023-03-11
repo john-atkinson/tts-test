0174944035550d8779bd24766548ae8a863047ee
John Atkinson <jatkinson@primeworks.io>
2023-03-11 05:04:06 -0800

    Initial Commit

---
+++ tts-test/tts_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  tts_testApp.swift
+//  tts-test
+//
+//  Created by John Atkinson on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct tts_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
