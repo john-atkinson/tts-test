d05fce6eb240e11d45b883b90e4c740bd7ac5f7b
John Atkinson <jatkinson@primeworks.io>
2023-03-18 19:20:17 -0700

    Spot detail and voice start/stop

---
+++ HotSpot/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  tts-test
+//
+//  Created by John Atkinson on 3/11/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        VStack {
+            Image("turtlerock")
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+        }
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
